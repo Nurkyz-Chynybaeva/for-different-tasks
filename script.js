0bf97a0a55b2670b3e3c4b7391f7e37945805013
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-06 21:11:07 +0600

    completed 3 of 10

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 let ul = document.createElement("ul");
 let li = document.createElement("li");
 
-
-
 li.append("Style #1");
 ul.append(li);
 li.style.border = "2px solid black";
@@ -19,9 +17,7 @@ document.body.append(ul);
 
 
 let ul2 = document.createElement("ul");
-let li2 = document.createElement("li");           // домашка
-
-
+let li2 = document.createElement("li");           
 
 li2.append("Style #2");
 ul2.append(li2);
@@ -31,5 +27,45 @@ li2.style.border = "2px solid pink";
 li2.style.width = "80px"
 li2.style.height = "30px"
 document.body.append(ul2);
+
+
+
+
+
+
+let ul3 = document.createElement("ul");
+let li3 = document.createElement("li");           
+
+li3.append("Style #3");
+ul3.append(li3);
+li3.style.textAlign = "center";
+li3.style.border = "5px dashed red";
+li3.style.width = "80px"
+li3.style.color = "red"
+li3.style.fontWeight ="800"
+li3.style.fontSize ="22px"
+li3.style.height = "30px"
+document.body.append(ul3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
