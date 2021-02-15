c8195b5187a6c7612ff2dc9ba1c47f782a20cfbb
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-15 22:52:34 +0600

    created and added button

--- script.js
+++ script.js
@@ -384,15 +384,15 @@ document.oncontextmenu = function(){
 let fruits = ["apple" , "watermelon" , "pomegranate" , "tangerine" , "bananas" , "kiwi"];
 let ul = document.createElement('ul');
 let input = document.querySelector("input");
+let btn = document.querySelector("button");
+document.body.append(ul);
+
 for (let i = 0; i < fruits.length; i++) {
     let li = document.createElement('li');
     li.addEventListener('click' , function(){
 
     })
 }
-    
-
-
 let button = document.createElement('button');
 button.innerText = '-'
 li.innerText = fruits[i];
@@ -402,8 +402,6 @@ button.addEventListener('click' , function(){
     li.remove();
 });
 
-
-document.body.append(ul);
 
 
 
