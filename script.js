e1ccbcc6658ad0a1a08ccdbd6eef8bd6ac3383b1
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-17 14:03:19 +0600

    countries

--- script.js
+++ script.js
@@ -382,17 +382,58 @@ document.oncontextmenu = function(){
 */
 
 
-let fruits = ["apple", "bananas","watermelon", "tangerine",  "grapes", "orange" ,"kiwi"];
+
+let fruits = ["apple" , "bananas" , "watermelon" , "tangerine" ,  "grapes" , "orange" , "kiwi"];
 let ul = document.createElement("ul");
 for (const fruit of fruits) {
     let li = document.createElement("li");
   let span = document.createElement('span');
+  span.addEventListener ('click', function(){
+    span.classList.toggle("done") 
+})
+
+
+let button = document.createElement("button");
+button.innerText = "-";
+span.innerText = fruit;
+ul.append(li);
+li.append(span);
+li.append(button);
+button.addEventListener("click", function () {
+  li.remove();
+
+});
 }
 
 
+document.body.append(ul);
 
 
+let input = document.querySelector("input");
+let button = document.querySelector("button");
 
+button.addEventListener("click", function () {
+    if (input.value) {
+        let li = document.createElement("li");
+        let span = document.createElement('span');
+        span.addEventListener ('click', function(){
+            span.classList.toggle("done") 
+})
+
+let button = document.createElement("button");
+    button.innerText = "-";
+    span.innerText = input.value
+    input.value = " ";
+    ul.append(li);
+    li.append(span);
+    li.append(button);
+
+
+    button.addEventListener("click", function () {
+        li.remove();
+      });
+    }
+  });
 
 
 
